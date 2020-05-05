2114ff43044118eee85ca7346aff2b9866247c9c
devindersingh79904 <devindersingh79904@gmail.com>
2020-05-05 16:23:53 +0530

    all activity words are added

--- app/src/main/java/com/example/miwoklanguage/FamilyActivity.java
+++ app/src/main/java/com/example/miwoklanguage/FamilyActivity.java
@@ -3,6 +3,9 @@ package com.example.miwoklanguage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+
+
+        ArrayList<Word> numbers = new ArrayList<Word>();
+
+        numbers.add(new Word("Father","әpә"));
+        numbers.add(new Word("Mother","әṭa"));
+        numbers.add(new Word("Son","angsi"));
+        numbers.add(new Word("Daughter","tune "));
+        numbers.add(new Word("older brother","taachi"));
+        numbers.add(new Word("younger brother","chalitti"));
+        numbers.add(new Word("older sister","teṭe"));
+        numbers.add(new Word("younger sister","kolliti"));
+        numbers.add(new Word("grandmother","ama"));
+        numbers.add(new Word("grandfather","paapa"));
+
+        WordAdapter items = new WordAdapter(this,numbers);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        listView.setAdapter(items);
     }
 }
--- app/src/main/java/com/example/miwoklanguage/PhrasesActivity.java
+++ app/src/main/java/com/example/miwoklanguage/PhrasesActivity.java
@@ -3,6 +3,9 @@ package com.example.miwoklanguage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+
+        ArrayList<Word> numbers = new ArrayList<Word>();
+
+        numbers.add(new Word("Where are you going?","minto wuksus"));
+        numbers.add(new Word("What is your name?","tinnә oyaase'nә"));
+        numbers.add(new Word("My name is...","oyaaset..."));
+        numbers.add(new Word("How are you feeling?","michәksәs?"));
+        numbers.add(new Word("I’m feeling good.","kuchi achit"));
+        numbers.add(new Word("Are you coming?","әәnәs'aa?"));
+        numbers.add(new Word("Yes, I’m coming.","hәә’ әәnәm"));
+        numbers.add(new Word("I’m coming.", "әәnәm"));
+        numbers.add(new Word("Let’s go.","yoowutis"));
+        numbers.add(new Word("Come here.","әnni'nem"));
+
+        WordAdapter items = new WordAdapter(this,numbers);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        listView.setAdapter(items);
     }
 }
